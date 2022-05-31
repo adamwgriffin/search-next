8d542de7305ba0b42e6743e35b9731773a1c1df3
Adam Griffin <adam.griffin@moxiworks.com>
2022-05-30 17:04:06 -0700

    Added Price fields

---
+++ components/form/InputFromToSeparator/InputFromToSeparator.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from 'next'
+import styles from './InputFromToSeparator.module.css'
+
+const InputFromToSeparator: NextPage = () => {
+  return (
+    <span className={styles.inputFromToSeparator}>—</span>
+  )
+}
+
+export default InputFromToSeparator
--- components/form/Price/Price.tsx
+++ components/form/Price/Price.tsx
@@ -1,12 +1,16 @@
 import type { NextPage } from 'next'
 import styles from './Price.module.css'
+import formStyles from '../../../styles/forms.module.css'
 import MenuButton from '../../MenuButton/MenuButton'
+import InputFromToSeparator from '../InputFromToSeparator/InputFromToSeparator'
 
 const Price: NextPage = () => {
   return (
     <MenuButton label="Price">
       <div className={styles.price}>
-        Price Range
+        <input type="text" placeholder='Min' className={formStyles.input} />
+        <InputFromToSeparator />
+        <input type="text" placeholder='Max' className={formStyles.input} />
       </div>
     </MenuButton>
   )
