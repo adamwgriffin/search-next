a6c7a4a4ad949845271954a3ffe3258b10100541
Adam Griffin <adam.griffin@moxiworks.com>
2022-05-30 18:36:47 -0700

    Added blur event handler

--- components/form/Filters/Filters.tsx
+++ components/form/Filters/Filters.tsx
@@ -7,6 +7,8 @@ import styles from './Filters.module.css'
 const Filters: NextPage = () => {
   return (
     <div className={styles.filters}>
+      {/* we can handle blur events on Price likes so once we're ready to wire this up: */}
+      {/* <Price onBlur={(e) => console.log("Blur triggered:", { [e.target.name]: +e.target.value })} /> */}
       <Price />
       <Beds countArr={[0, 1, 2, 3, 4, 5]} />
       <More />
--- components/form/Price/Price.tsx
+++ components/form/Price/Price.tsx
@@ -1,16 +1,33 @@
 import type { NextPage } from 'next'
+import { FocusEventHandler } from 'react'
 import styles from './Price.module.css'
 import formStyles from '../../../styles/forms.module.css'
 import MenuButton from '../../MenuButton/MenuButton'
 import InputFromToSeparator from '../InputFromToSeparator/InputFromToSeparator'
 
-const Price: NextPage = () => {
+interface PriceProps {
+  onBlur?: FocusEventHandler<HTMLInputElement>
+}
+
+const Price: NextPage<PriceProps> = (props) => {
   return (
     <MenuButton label="Price">
       <div className={styles.price}>
-        <input type="text" placeholder='Min' className={formStyles.input} />
+        <input
+          type="text"
+          placeholder='Min'
+          className={formStyles.input}
+          name="pricemin"
+          onBlur={props.onBlur}
+        />
         <InputFromToSeparator />
-        <input type="text" placeholder='Max' className={formStyles.input} />
+        <input
+          type="text"
+          placeholder='Max'
+          className={formStyles.input}
+          name="pricemax"
+          onBlur={props.onBlur}
+        />
       </div>
     </MenuButton>
   )
