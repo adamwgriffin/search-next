a8b01df51f867653b9558cd42335695e8326d92d
Adam Griffin <adam.griffin@moxiworks.com>
2022-08-06 00:01:48 -0700

    Removed location data
    It's not being used for anything

--- components/Search/Search.tsx
+++ components/Search/Search.tsx
@@ -7,35 +7,6 @@ import SearchResults from '../SearchResults/SearchResults'
 import ListingMap from '../ListingMap/ListingMap'
 import styles from './Search.module.css'
 
-const defaultLocations = [
-  {
-    position: {
-      lat: 48.160910,
-      lng: 16.383330,
-    },
-  },
-  {
-    position: {
-      lat: 48.174270,
-      lng: 16.329620,
-    },
-  },
-  // prague
-  {
-    position: {
-      lat: 50.075,
-      lng: 14.4378,
-    },
-  },
-  // budapest
-  {
-    position: {
-      lat: 47.4979,
-      lng: 19.0402,
-    },
-  }
-]
-
 const initializeGoogleMaps = async (setGoogleLoaded: Function) => {
   // we wait to render the content in the view that depends on the google maps api until the api is loaded, that way the
   // "google" variable will be defined for any of the components need to create instances of google maps classes, e.g.,
@@ -69,7 +40,6 @@ const searchTemplate = () => {
 
 const Search: NextPage = () => {
   const [googleLoaded, setGoogleLoaded] = useState(false)
-  const [locations, setLocations] = useState(defaultLocations)
 
   useEffect(() => {
     initializeGoogleMaps(setGoogleLoaded)
