8fa5c2b212ea37c9db7895eefc344f39ab10b935
Adam Griffin <adam.griffin@moxiworks.com>
2022-11-16 13:43:00 -0800

    Removed DefaultSortOptions
    I don't plan on using it

--- lib/constants/filter_constants.ts
+++ lib/constants/filter_constants.ts
@@ -1,6 +1,3 @@
-import pick from 'lodash/pick'
-import { sortByEnum } from './search_param_constants'
-
 // range numbers for SqftOptionRanges represent: [start, end, increment by]. the end number is
 // exclusive, so we have to add 1
 export const SqftOptionRanges = Object.freeze([
@@ -11,13 +8,3 @@ export const SqftOptionRanges = Object.freeze([
 export const LotSizeSqftOptions = Object.freeze([2000, 4500, 6500, 8000, 10890, 21780])
 
 export const LotSizeAcreOptions = Object.freeze([.25, .5, 1, 2, 3, 4, 5, 10, 40, 100])
-
-export const DefaultSortOptions = Object.freeze(pick(sortByEnum, [
-  'price_desc',
-  'price_asc',
-  'listing_date_desc',
-  'beds_desc',
-  'baths_desc',
-  'total_square_footage_desc',
-  'distance_from_user_lat_lon_asc'
-]))
