79bee6f838527e84751057a1122af494869af1e2
Adam Griffin <adam.griffin@moxiworks.com>
2022-11-22 11:06:44 -0800

    Make Popup a link

--- components/map/ListingMarkerPopupCard/ListingMarkerPopupCard.tsx
+++ components/map/ListingMarkerPopupCard/ListingMarkerPopupCard.tsx
@@ -16,23 +16,32 @@ const ListingMarkerPopupCard: NextPage<ListingMarkerPopupCardProps> = ({
   listing
 }) => {
   return (
-    <div className={styles.listingMarkerPopupCard}>
-      <img className={styles.image} src={listing.image[0].small_url}></img>
-      <div className={styles.details}>
-        <div className={styles.price}>{formatPrice(listing)}</div>
-        <div className={styles.stats}>
-          <div className={styles.beds}>{listing.bedrooms}bd</div>
-          <div className={styles.baths}>{getBathrooms(listing)}ba</div>
-          <div className={styles.size}>{formatSqft(listing)} sqft</div>
-        </div>
-        <div className={styles.address}>
-          <div className={styles.addressLine1}>{listing.location.address}</div>
-          <div className={styles.addressLine2}>
-            {cityStateZip(listing.location)}
+    <a
+      target='_blank'
+      rel='noopener noreferrer'
+      href='about:blank'
+      className={styles.link}
+    >
+      <div className={styles.listingMarkerPopupCard}>
+        <img className={styles.image} src={listing.image[0].small_url}></img>
+        <div className={styles.details}>
+          <div className={styles.price}>{formatPrice(listing)}</div>
+          <div className={styles.stats}>
+            <div className={styles.beds}>{listing.bedrooms}bd</div>
+            <div className={styles.baths}>{getBathrooms(listing)}ba</div>
+            <div className={styles.size}>{formatSqft(listing)} sqft</div>
+          </div>
+          <div className={styles.address}>
+            <div className={styles.addressLine1}>
+              {listing.location.address}
+            </div>
+            <div className={styles.addressLine2}>
+              {cityStateZip(listing.location)}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </a>
   )
 }
 
