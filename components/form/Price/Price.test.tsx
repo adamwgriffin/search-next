7d1025ec0999a21a8464d7ada3c85187d42513a0
Adam Griffin <adam.griffin@moxiworks.com>
2022-11-22 18:05:38 -0800

    Fixed Price test

--- components/form/Price/Price.test.tsx
+++ components/form/Price/Price.test.tsx
@@ -4,12 +4,12 @@ import Price from './Price'
 describe('Price', () => {
   
   it('Includes a Min Price field', () => {
-    render(<Price />)
+    render(<Price priceRange={{ pricemin: null, pricemax: null }} />)
     expect(screen.getByRole('textbox', { name: 'Min Price' })).toBeInTheDocument()
   })
 
   it('Includes a Max Price field', () => {
-    render(<Price />)
+    render(<Price priceRange={{ pricemin: null, pricemax: null }}  />)
     expect(screen.getByRole('textbox', { name: 'Max Price' })).toBeInTheDocument()
   })
 
