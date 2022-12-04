301bd4cdd4ff9a3eb064a6face091c1a17e07e14
Adam Griffin <adam.griffin@moxiworks.com>
2022-12-03 23:47:18 -0800

    Added GoogleMapsProvider to ListingDetail
    We will probably need it on the listing detail page in order to show a map of the listing location.

--- containers/ListingDetail/ListingDetail.tsx
+++ containers/ListingDetail/ListingDetail.tsx
@@ -5,6 +5,8 @@ import {
   selectListing
 } from '../../store/listingDetail/listingDetailSlice'
 import { useAppSelector, useAppDispatch } from '../../hooks'
+import GoogleMapsProvider from '../../context/google_maps_context'
+import { AppGoogleMapsLoaderOptions } from '../../config/googleMapsOptions'
 import {
   formatPrice,
   getBathrooms,
@@ -28,82 +30,84 @@ const ListingDetail: NextPage<ListingDetailProps> = ({ listingID }) => {
   }, [listingID])
 
   return (
-    <div>
-      {/* <header className={styles.header}>Header</header> */}
-      {listing && (
-        <div className={styles.listingDetail}>
+    <GoogleMapsProvider loaderOptions={AppGoogleMapsLoaderOptions}>
+      <div>
+        {/* <header className={styles.header}>Header</header> */}
+        {listing && (
+          <div className={styles.listingDetail}>
 
-          <div className={listing.pstatus_id === 1 ? styles.statusActive : styles.status}>
-            {listing.status_name_for_view}
-          </div>
-
-          <div className={styles.gallery}>
-            <ListingMainImage
-              image={listing.images[0]}
-              location={listing.location}
-              size='full'
-              style={{
-                objectFit: 'cover',
-                cursor: 'pointer',
-                width: '100%',
-                height: '30rem',
-                borderRadius: '0.5rem'
-              }}
-            />
-          </div>
-
-          <div className={styles.price}>{formatPrice(listing)}</div>
-
-          <div title='Neighborhood' className={styles.neighborhood}>
-            {listing.neighborhood}
-          </div>
-
-          <address className={styles.address}>
-            <div className={styles.addressLine1}>
-              {listing.location.address}
+            <div className={listing.pstatus_id === 1 ? styles.statusActive : styles.status}>
+              {listing.status_name_for_view}
             </div>
-            <div className={styles.addressLine2}>
-              {cityStateZip(listing.location)}
+
+            <div className={styles.gallery}>
+              <ListingMainImage
+                image={listing.images[0]}
+                location={listing.location}
+                size='full'
+                style={{
+                  objectFit: 'cover',
+                  cursor: 'pointer',
+                  width: '100%',
+                  height: '30rem',
+                  borderRadius: '0.5rem'
+                }}
+              />
             </div>
-          </address>
 
-          <div className={styles.stats}>
-            <div className={styles.beds}>{listing.bedrooms} Bed</div>
-            <div className={styles.baths}>{getBathrooms(listing)} Bath</div>
-            <div className={styles.size}>{formatSqft(listing)} sqft</div>
-          </div>
+            <div className={styles.price}>{formatPrice(listing)}</div>
 
-          <div className={styles.description}>
-            <h4 className={styles.heading}>Description</h4>
-            <p>{listing.comments}</p>
-          </div>
+            <div title='Neighborhood' className={styles.neighborhood}>
+              {listing.neighborhood}
+            </div>
 
-          <div className={styles.details}>
-            <h4 className={styles.heading}>Property Details</h4>
-            <ul className={styles.detailsList}>
-              <li>
-                <div className={styles.detailsName}>Property Type</div>
-                <div>{listing.property_type}</div>
-              </li>
-              <li>
-                <div className={styles.detailsName}>Time on Site</div>
-                <div>{`${listing.days_on_market.toLocaleString()} ${
-                  listing.days_on_market > 1 ? 'days' : 'day'
-                }`}</div>
-              </li>
-              <li>
-                <div className={styles.detailsName}>Year Built</div>
-                <div>{listing.year_build}</div>
-              </li>
-              <li>
-                <div className={styles.detailsName}>MLS Number</div>
-                <div>{listing.mlsnumber}</div>
-              </li>
-            </ul>
+            <address className={styles.address}>
+              <div className={styles.addressLine1}>
+                {listing.location.address}
+              </div>
+              <div className={styles.addressLine2}>
+                {cityStateZip(listing.location)}
+              </div>
+            </address>
+
+            <div className={styles.stats}>
+              <div className={styles.beds}>{listing.bedrooms} Bed</div>
+              <div className={styles.baths}>{getBathrooms(listing)} Bath</div>
+              <div className={styles.size}>{formatSqft(listing)} sqft</div>
+            </div>
+
+            <div className={styles.description}>
+              <h4 className={styles.heading}>Description</h4>
+              <p>{listing.comments}</p>
+            </div>
+
+            <div className={styles.details}>
+              <h4 className={styles.heading}>Property Details</h4>
+              <ul className={styles.detailsList}>
+                <li>
+                  <div className={styles.detailsName}>Property Type</div>
+                  <div>{listing.property_type}</div>
+                </li>
+                <li>
+                  <div className={styles.detailsName}>Time on Site</div>
+                  <div>{`${listing.days_on_market.toLocaleString()} ${
+                    listing.days_on_market > 1 ? 'days' : 'day'
+                  }`}</div>
+                </li>
+                <li>
+                  <div className={styles.detailsName}>Year Built</div>
+                  <div>{listing.year_build}</div>
+                </li>
+                <li>
+                  <div className={styles.detailsName}>MLS Number</div>
+                  <div>{listing.mlsnumber}</div>
+                </li>
+              </ul>
+            </div>
           </div>
-        </div>
-      )}
-    </div>
+        )}
+      </div>
+    </GoogleMapsProvider>
   )
 }
 
