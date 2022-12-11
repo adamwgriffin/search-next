60487124e5c2a0ad00910db4d3a74aa92737473e
Adam Griffin <adam.griffin@moxiworks.com>
2022-12-10 23:20:19 -0800

    Added rounded corners to ListingCardLoader

--- components/listings/ListingCardLoader/ListingCardLoader.tsx
+++ components/listings/ListingCardLoader/ListingCardLoader.tsx
@@ -9,13 +9,13 @@ const ListingCardLoader: NextPage = () => (
       width={'100%'}
       height={'100%'}
     >
-      <rect x="0" y="0" width="100%" height="208" />      
-      <rect x="16" y="226" width="50%" height="24" />
-      <rect x="16" y="262" width="36" height="16" />
-      <rect x="62" y="262" width="36" height="16" />
-      <rect x="108" y="262" width="56" height="16" />
-      <rect x="16" y="290" width="60%" height="16" />
-      <rect x="16" y="310" width="60%" height="16" />
+      <rect x="0" y="0" width="100%" height="208" rx='0.5rem'/>      
+      <rect x="16" y="226" width="50%" height="24" rx='0.375rem' />
+      <rect x="16" y="262" width="36" height="16" rx='0.375rem' />
+      <rect x="62" y="262" width="36" height="16" rx='0.375rem' />
+      <rect x="108" y="262" width="56" height="16" rx='0.375rem' />
+      <rect x="16" y="290" width="60%" height="16" rx='0.375rem' />
+      <rect x="16" y="310" width="60%" height="16" rx='0.375rem' />
     </ContentLoader>
   </div>
 )
