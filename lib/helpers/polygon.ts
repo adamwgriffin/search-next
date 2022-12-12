ed24e4fcac93b4f8fb213b4839e0d34c321485b7
Adam Griffin <adam.griffin@moxiworks.com>
2022-12-11 22:59:22 -0800

    Formatting

--- lib/helpers/polygon.ts
+++ lib/helpers/polygon.ts
@@ -1,32 +1,35 @@
-import type { GeoLayerCoordinates, GeoJSONCoordinates } from '../../store/listingMap/listingMapSlice'
+import type {
+  GeoLayerCoordinates,
+  GeoJSONCoordinates
+} from '../../store/listingMap/listingMapSlice'
 /*
 we need to transform the geojson we get from the service into a shape that works for the Polygon class we need to use
 it for. we get the data as:
 [
   [
     [
-      [-122.510645, 47.228309],
-      [-122.506443, 47.226519],
-      ...
-    ]
-  ],
-  [
-    [
-      [-122.510645, 47.228309],
-      ...
+      [
+        -122.510645,
+        47.228309
+      ],
+      [
+        -122.506443,
+        47.226519
+      ]
     ]
   ]
 ]
 but instead we need:
 [
   [
-    { lat: 47.228309, lng: -122.510645 },
-    { lat: 47.226519, lng: -122.506443 },
-    ...
-  ],
-  [
-    { lat: 47.228309, lng: -122.510645 },
-    ...
+    {
+      lat: 47.228309,
+      lng: -122.510645
+    },
+    {
+      lat: 47.226519,
+      lng: -122.506443
+    },
   ]
 ]
 */
@@ -42,8 +45,10 @@ export const convertGeojsonCoordinatesToPolygonPaths = (
 
 // most examples use polygon.getPaths() to extend the bounds, but that data is the same as the geojson coordinates we
 // used to create the polygon paths, so we might as well just use that data since we already have it
-export const getGeoLayerBounds = (geoLayerCoordinates:GeoLayerCoordinates) => {
+export const getGeoLayerBounds = (geoLayerCoordinates: GeoLayerCoordinates) => {
   const bounds = new google.maps.LatLngBounds()
-  geoLayerCoordinates.forEach(latLngArr => latLngArr.forEach(latLng => bounds.extend(latLng)))
+  geoLayerCoordinates.forEach((latLngArr) =>
+    latLngArr.forEach((latLng) => bounds.extend(latLng))
+  )
   return bounds.toJSON()
 }
