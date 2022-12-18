0dc81b9ceee78b12ee2aeb980d3bb2117e8c354f
Adam Griffin <adam.griffin@moxiworks.com>
2022-12-17 20:16:36 -0800

    PropertyStatusTypes are not used

--- lib/constants/listing_constants.ts
+++ lib/constants/listing_constants.ts
@@ -1,21 +1,3 @@
-export const PropertyStatusTypes = Object.freeze({
-  active: 1,
-  pending: 2,
-  coming_soon: 11,
-  temp_off_market: 8,
-  contingent: 3,
-  expired: 5,
-  sale_fail: 6,
-  canceled: 4,
-  other: 10,
-  short_sale: 7,
-  sold: 9
-})
-
-export const PropertyStatusTypeIds = Object.freeze(
-  Object.values(PropertyStatusTypes).sort((a, b) => a - b)
-)
-
 export const ListingImageSizeEnum = Object.freeze({
   raw: { width: 1600, height: 1600 },
   gallery: { width: 1600, height: 1600 },
