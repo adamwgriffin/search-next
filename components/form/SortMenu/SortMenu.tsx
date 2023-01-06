dcc958a57e1376893f413dfa2d6fd819d7a853f3
Adam Griffin <adam.griffin@moxiworks.com>
2023-01-05 23:29:32 -0800

    Refactored SortMenu
    Using new dropdown instead of select because it's more consistent with the overall theme and looks the same across different browsers.

--- components/form/SortMenu/SortMenu.tsx
+++ components/form/SortMenu/SortMenu.tsx
@@ -1,44 +1,58 @@
 import type { NextPage } from 'next'
 import type { SortById } from '../../../lib/listing_service_params_types'
+import { useToggle } from 'react-use'
 import styles from './SortMenu.module.css'
 import { sortByEnum } from '../../../lib/listing_service_params'
+import Dropdown from '../Dropdown/Dropdown'
+import CheckIcon from '../../icons/CheckIcon/CheckIcon'
 
 export interface SortMenuProps {
   sortBy: SortById
-  onChange: (sortById: SortById) => void
+  onChange?: (sortById: SortById) => void
 }
 
-const SortMenu: NextPage<SortMenuProps> = ({ sortBy=sortByEnum.listing_date_desc, onChange }) => {
+const SortByIdLabels: Partial<Record<SortById, string>> = {
+  [sortByEnum.listing_date_desc]: 'Newest',
+  [sortByEnum.price_asc]: 'Price (Lo-Hi)',
+  [sortByEnum.price_desc]: 'Price (Hi-Lo)',
+  [sortByEnum.beds_desc]: 'Beds',
+  [sortByEnum.baths_desc]: 'Baths',
+  [sortByEnum.total_square_footage_desc]: 'Square Feet'
+}
+
+const SortMenu: NextPage<SortMenuProps> = ({
+  sortBy = sortByEnum.listing_date_desc,
+  onChange
+}) => {
+  const [open, toggleMenu] = useToggle(false)
+
+  const handleClick = (sortById: SortById) => {
+    toggleMenu(false)
+    onChange?.(sortById)
+  }
+
   return (
-    <div className={styles.sortMenu}>
-      <div className={styles.sortLabel}>
-        Sort:
-      </div>
-      <select
-        className={styles.sortByDropdown}
-        value={sortBy}
-        onChange={(e) => onChange(+e.target.value as SortById)}
-      >
-        <option value={sortByEnum.listing_date_desc}>
-          Newest
-        </option>
-        <option value={sortByEnum.price_asc}>
-          Price (Lo-Hi)
-        </option>
-        <option value={sortByEnum.price_desc}>
-          Price (Hi-Lo)
-        </option>
-        <option value={sortByEnum.beds_desc}>
-          Beds
-        </option>
-        <option value={sortByEnum.baths_desc}>
-          Baths
-        </option>
-        <option value={sortByEnum.total_square_footage_desc}>
-          Square Feet
-        </option>
-      </select>
-    </div>
+    <Dropdown
+      open={open}
+      label={`Sort: ${SortByIdLabels[sortBy]}`}
+      condensed
+      alignRight
+      onClick={toggleMenu}
+      onClickAway={() => toggleMenu(false)}
+    >
+      <ul className={styles.menu}>
+        {Object.entries(SortByIdLabels).map(([id, label]) => (
+          <li
+            key={id}
+            onClick={() => handleClick(Number(id) as SortById)}
+            className={styles.menuItem}
+          >
+            <div>{sortBy === Number(id) && <CheckIcon />}</div>
+            <div>{label}</div>
+          </li>
+        ))}
+      </ul>
+    </Dropdown>
   )
 }
 
---
+++ components/icons/CheckIcon/CheckIcon.tsx
@@ -0,0 +1,19 @@
+import type { NextPage } from 'next'
+
+const CheckIcon = () => {
+  return (
+    <svg
+      fill="#212e35"
+      width="16"
+      height="16"
+      viewBox="0 0 1024 1024"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M409.35 675.135l387.924-387.924 48.346 48.346-436.086 436.11-200.226-200.226 48.174-48.174 151.868 151.868z"
+      ></path>
+    </svg>
+  )
+}
+
+export default CheckIcon
