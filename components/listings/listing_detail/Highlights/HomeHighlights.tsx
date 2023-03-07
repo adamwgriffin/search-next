55175dac7108196d8e56e402338e798af7d291b0
Adam Griffin <adam.griffin@moxiworks.com>
2023-03-06 22:53:32 -0800

    Format HomeHighlights for mobile

--- components/listings/listing_detail/Highlights/HomeHighlights.tsx
+++ components/listings/listing_detail/Highlights/HomeHighlights.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next'
 import type { ListingDetailListing } from '../../../../lib/types/listing_types'
-import css from 'styled-jsx/css'
+import styles from './HomeHighlights.module.css'
 
 export interface HomeHighlightsProps {
   listing: ListingDetailListing
@@ -10,17 +10,16 @@ const HomeHighlights: NextPage<HomeHighlightsProps> = ({
   listing
 }) => {
   return (
-    <>
       <div>
-        <h4 className='heading'>Home Highlights</h4>
-        <ul className='detailsList'>
+        <h4 className={styles.heading}>Home Highlights</h4>
+        <ul className={styles.detailsList}>
           <li>
-            <div className='detailsName'>Property Type</div>
+            <div className={styles.detailsName}>Property Type</div>
             <div>{listing.property_type}</div>
           </li>
           {listing.days_on_market && (
             <li>
-              <div className='detailsName'>Time on Site</div>
+              <div className={styles.detailsName}>Time on Site</div>
               <div>
                 {`${listing.days_on_market.toLocaleString()} ${
                   listing.days_on_market > 1 ? 'days' : 'day'
@@ -29,43 +28,17 @@ const HomeHighlights: NextPage<HomeHighlightsProps> = ({
             </li>
           )}
           <li>
-            <div className='detailsName'>Year Built</div>
+            <div className={styles.detailsName}>Year Built</div>
             <div>{listing.year_build}</div>
           </li>
           <li>
-            <div className='detailsName'>MLS Number</div>
+            <div className={styles.detailsName}>MLS Number</div>
             <div>{listing.mlsnumber}</div>
           </li>
         </ul>
       </div>
-      <style jsx>{styles}</style>
-    </>
+
   )
 }
-
-const styles = css`
-  .heading {
-    margin-top: 0;
-  }
-
-  .detailsList {
-    display: grid;
-    grid-template-columns: repeat(2, 1fr);
-    grid-gap: 1rem;
-    list-style: none;
-    margin: 0;
-    padding: 0;
-  }
-
-  .detailsList > li {
-    display: grid;
-    grid-template-columns: minmax(100px, 34%) auto;
-    grid-template-rows: auto auto;
-  }
-
-  .detailsName {
-    color: #767676;
-  }
-`
 
 export default HomeHighlights
