9f6e66bf026a1c0a73fc01108174c118b71cde4b
Adam Griffin <adam.griffin@moxiworks.com>
2023-03-12 17:53:49 -0700

    Made search results scroll to top when new search runs

--- containers/Search/Search.tsx
+++ containers/Search/Search.tsx
@@ -1,10 +1,11 @@
 import type { NextPage } from 'next'
 import styles from './Search.module.css'
+import { useRef, useEffect } from 'react'
 import { useEffectOnce } from 'react-use'
 import { useAppSelector, useAppDispatch } from '../../hooks'
 import GoogleMapsProvider from '../../context/google_maps_context'
 import { selectViewType } from '../../store/application/applicationSlice'
-import { doGeospatialGeocodeSearch } from '../../store/listingSearch/listingSearchSlice'
+import { doGeospatialGeocodeSearch, selectListingSearchRunning } from '../../store/listingSearch/listingSearchSlice'
 import Header from '../../containers/Header/Header'
 import SearchResults from '../../containers/SearchResults/SearchResults'
 import ListingMap from '../../components/map/ListingMap/ListingMap'
@@ -13,6 +14,8 @@ import { AppGoogleMapsLoaderOptions } from '../../config/googleMapsOptions'
 const Search: NextPage = () => {
   const dispatch = useAppDispatch()
   const viewType = useAppSelector(selectViewType)
+  const listingSearchRunning = useAppSelector(selectListingSearchRunning)
+  const searchResultsRef = useRef<null | HTMLDivElement>(null)
   const resultsClassName =
     viewType === 'list' ? styles.resultsListView : styles.resultsMapView
   
@@ -21,6 +24,12 @@ const Search: NextPage = () => {
     dispatch(doGeospatialGeocodeSearch())
   })
 
+  useEffect(() => {
+    if (searchResultsRef?.current?.scrollTop) {
+      searchResultsRef.current.scrollTop = 0;
+    }
+  }, [listingSearchRunning])
+
   return (
     <GoogleMapsProvider loaderOptions={AppGoogleMapsLoaderOptions}>
       <div className={styles.search}>
@@ -28,7 +37,7 @@ const Search: NextPage = () => {
           <Header />
         </div>
         <div className={resultsClassName}>
-          <div className={styles.searchResults}>
+          <div ref={searchResultsRef} className={styles.searchResults}>
             <SearchResults />
           </div>
           <div className={styles.listingMap}>
