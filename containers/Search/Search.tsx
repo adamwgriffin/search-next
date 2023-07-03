61b7636d609b121b75bc756d58d86e5b11e881e8
Adam Griffin <1782209+adamwgriffin@users.noreply.github.com>
2023-07-03 00:19:31 -0700

    Moved all code related to url handling into homes
    Using props to pass state back and forth between homes and Search seems needlessly complicated. Moved it all into homes in order to simplify things. Updated comments again so that each step in the process has a more complete explanation.

--- containers/Search/Search.tsx
+++ containers/Search/Search.tsx
@@ -1,51 +1,27 @@
 import type { NextPage } from 'next'
-import type { FiltersState } from '../../store/filters/filtersTypes'
 import { useRef, useEffect } from 'react'
-import { useAppSelector, useAppDispatch } from '../../hooks'
+import { useAppSelector } from '../../hooks'
 import GoogleMapsProvider from '../../context/google_maps_context'
 import { AppGoogleMapsLoaderOptions } from '../../config/googleMapsOptions'
-import { setFilters } from '../../store/filters/filtersSlice'
-import { selectSearchState } from '../../store/filters/filtersSelectors'
 import { selectViewType } from '../../store/application/applicationSlice'
-import { searchNewLocation } from '../../store/listingSearch/listingSearchSlice'
 import { selectListingSearchRunning } from '../../store/listingSearch/listingSearchSelectors'
 import styles from './Search.module.css'
 import Header from '../../containers/Header/Header'
 import SearchResults from '../../containers/SearchResults/SearchResults'
 import ListingMap from '../../components/map/ListingMap/ListingMap'
 
-export interface SearchProps {
-  searchState?: Partial<FiltersState>
-  onListingSearchRunning?: (state: Partial<FiltersState>) => void
-}
-
-const Search: NextPage<SearchProps> = ({
-  searchState,
-  onListingSearchRunning
-}) => {
-  const dispatch = useAppDispatch()
+const Search: NextPage = () => {
   const viewType = useAppSelector(selectViewType)
   const listingSearchRunning = useAppSelector(selectListingSearchRunning)
-  const currentSearchState = useAppSelector(selectSearchState)
   const searchResultsRef = useRef<HTMLDivElement>(null)
   const resultsClassName =
     viewType === 'list' ? styles.resultsListView : styles.resultsMapView
 
   useEffect(() => {
-    if (searchState) {
-      dispatch(setFilters(searchState))
-      dispatch(searchNewLocation())
+    if (listingSearchRunning && searchResultsRef?.current?.scrollTop) {
+      searchResultsRef.current.scrollTop = 0
     }
-  }, [searchState, dispatch])
-
-  useEffect(() => {
-    if (listingSearchRunning) {
-      if (searchResultsRef?.current?.scrollTop) {
-        searchResultsRef.current.scrollTop = 0
-      }
-      onListingSearchRunning?.(currentSearchState)
-    }
-  }, [listingSearchRunning, currentSearchState, onListingSearchRunning])
+  }, [listingSearchRunning])
 
   return (
     <GoogleMapsProvider loaderOptions={AppGoogleMapsLoaderOptions}>
--- pages/homes.tsx
+++ pages/homes.tsx
@@ -1,6 +1,6 @@
 import type { FiltersState } from '../store/filters/filtersTypes'
 import type { NextPage } from 'next'
-import { useState, useCallback } from 'react'
+import { useState, useCallback, useEffect } from 'react'
 import { useMount, useUnmount, useEvent } from 'react-use'
 import { useRouter } from 'next/router'
 import isEqual from 'lodash/isEqual'
@@ -9,9 +9,14 @@ import {
   listingSearchURLParamsToSearchState,
   searchStateToListingSearchURLParams
 } from '../lib/url'
-import { clearFilters } from '../store/filters/filtersSlice'
-import { selectInitialSearchComplete } from '../store/listingSearch/listingSearchSelectors'
+import { clearFilters, setFilters } from '../store/filters/filtersSlice'
+import {
+  selectInitialSearchComplete,
+  selectListingSearchRunning
+} from '../store/listingSearch/listingSearchSelectors'
 import Search from '../containers/Search/Search'
+import { selectSearchState } from '../store/filters/filtersSelectors'
+import { searchNewLocation } from '../store/listingSearch/listingSearchSlice'
 
 export interface SearchPageProps {
   params: string[]
@@ -20,58 +25,63 @@ export interface SearchPageProps {
 const SearchPage: NextPage<SearchPageProps> = () => {
   const router = useRouter()
   const dispatch = useAppDispatch()
+  const listingSearchRunning = useAppSelector(selectListingSearchRunning)
   const initialSearchComplete = useAppSelector(selectInitialSearchComplete)
-  const [searchState, setSearchState] = useState<Partial<FiltersState>>()
-  const [previousState, setPreviousState] = useState<Partial<FiltersState>>()
+  const searchState = useAppSelector(selectSearchState)
+  const [previousSearchState, setPreviousSearchState] =
+    useState<Partial<FiltersState>>()
 
-  // TODO: will eventually validate query with zod and throw away any invalid params so that we can guaruntee that this
-  // function will always be passed valid params
-  const setFiltersStateToUrlSearchParams = useCallback(() => {
-    setSearchState(
-      listingSearchURLParamsToSearchState(
-        new URLSearchParams(window.location.search)
-      )
+  // get the browser url query string, convert it to a state object, use it to set the state, then run a new search base
+  // on that state
+  const runNewSearchWithURLSearchParams = useCallback(() => {
+    // TODO: will eventually validate query params with zod and throw away any that are invalid, so that we can
+    // guaruntee that this function will always be passed valid data
+    const searchState = listingSearchURLParamsToSearchState(
+      new URLSearchParams(window.location.search)
     )
-  }, [setSearchState])
+    dispatch(setFilters(searchState))
+    dispatch(searchNewLocation())
+  }, [dispatch])
 
-  // get the browser url, convert it to a state object and use it to set app state to match the state encoded in the url
-  // params when the page first loads. the filtersState variable that holds this object is passed as a prop to <Search>
-  // which uses it to actually set the state in the store.
-  useMount(setFiltersStateToUrlSearchParams)
+  // run a search based on the url when the page first loads.
+  useMount(runNewSearchWithURLSearchParams)
 
   // if the user clicks the back or forward button in the browser, we want to get the url that was loaded from the
-  // previous/next part of the browser history and then set the app state to match the state represented in the url
-  // params. the "popstate" event is triggered whenever the history is changed by the user this way.
-  useEvent('popstate', setFiltersStateToUrlSearchParams)
+  // previous/next part of the browser history and then run a new search to match the url params. the "popstate" event
+  // is triggered whenever the history is changed by the user in this way.
+  useEvent('popstate', runNewSearchWithURLSearchParams)
+
+  // each time the user updates the filters, or searches for a new location, we want to update the url to reflect the
+  // new search. we're checking initialSearchComplete because we don't need to change the url if we just used it to
+  // set the FiltersState and run the initial search in the useMount callback above. it's only on subsequent runs that
+  // we would want to update the url to reflect whatever filters the user has changed. doing this prevents us from
+  // adding a duplicate url to the browser history, which can make if seem as if the back button doesn't work if the
+  // user clicks it after the search page first loads
+  useEffect(() => {
+    if (
+      listingSearchRunning &&
+      initialSearchComplete &&
+      !isEqual(searchState, previousSearchState)
+    ) {
+      router.push({
+        pathname: router.pathname,
+        query: searchStateToListingSearchURLParams(searchState)
+      })
+    }
+    setPreviousSearchState(searchState)
+  }, [
+    router,
+    initialSearchComplete,
+    searchState,
+    previousSearchState,
+    listingSearchRunning
+  ])
 
   // reset the filters when the user navigates away from the search page, otherwise subsequest search from the home page
   // may include them
   useUnmount(() => dispatch(clearFilters()))
 
-  const handleOnListingSearchRunning = useCallback(
-    (state: Partial<FiltersState>) => {
-      // checking initialSearchComplete since we don't need to change the url if we just used it to set the FiltersState
-      // and run the initial search. it's only on subsequent runs that we would want to update the url to reflect
-      // whatever filters the user has changed. doing this prevents us from adding a duplicate url to the browser
-      // history, which can make if seem as if the back button doesn't work if the user clicks it after the search page
-      // first loads
-      if (initialSearchComplete && !isEqual(state, previousState)) {
-        router.push({
-          pathname: router.pathname,
-          query: searchStateToListingSearchURLParams(state)
-        })
-      }
-      setPreviousState(state)
-    },
-    [router, initialSearchComplete, previousState]
-  )
-
-  return (
-    <Search
-      searchState={searchState}
-      onListingSearchRunning={handleOnListingSearchRunning}
-    />
-  )
+  return <Search />
 }
 
 export default SearchPage
