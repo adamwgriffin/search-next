c5357ca43cd1d4eedd7c5203c4dee69ed7a88421
Adam Griffin <1782209+adamwgriffin@users.noreply.github.com>
2023-07-02 22:36:51 -0700

    Added some links to home page
    Just to test things out

--- containers/Home/Home.tsx
+++ containers/Home/Home.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import GoogleMapsProvider from '../../context/google_maps_context'
 import { AppGoogleMapsLoaderOptions } from '../../config/googleMapsOptions'
+import Link from 'next/link'
 import styles from './Home.module.css'
 
 const Home: NextPage = () => {
@@ -9,6 +10,14 @@ const Home: NextPage = () => {
       <div className={styles.home}>
         <h1>Home Page</h1>
         <p>Welcome!</p>
+        <p>
+          <Link href='/homes'>Search Fremont</Link>
+        </p>
+        <p>
+          <Link href='/homes?location=Ballard--Seattle--WA--USA&beds-min=2&baths-min=1'>
+            Search Ballard 2+bd 1+ba
+          </Link>
+        </p>
       </div>
     </GoogleMapsProvider>
   )
