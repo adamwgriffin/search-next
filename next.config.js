1c1aac0d4dabb90788403c30912766b0c7cb1e31
Adam Griffin <1782209+adamwgriffin@users.noreply.github.com>
2023-12-28 21:57:54 -0800

    Added config for whitelisting login images
    For the NextJS <Image> component.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        hostname: 'lh3.googleusercontent.com'
+      },
+      {
+        hostname: 'avatars.githubusercontent.com'
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
