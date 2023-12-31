ce15043b6a7dbcbd3e52e15980022b6c19961a99
Adam Griffin <1782209+adamwgriffin@users.noreply.github.com>
2023-12-30 21:37:00 -0800

    Added current_user route

---
+++ app/api/current_user/route.ts
@@ -0,0 +1,24 @@
+import { NextResponse } from 'next/server'
+import { getServerSession } from 'next-auth/next'
+import authOptions from '../../../lib/auth/auth_options'
+import prisma from '../../../lib/prismadb'
+
+export async function GET() {
+  const session = await getServerSession(authOptions)
+  if (!session?.user) {
+    return null
+  }
+  const currentUser = await prisma.user.findUnique({
+    where: {
+      id: session.user.id
+    },
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      image: true,
+      favoriteIds: true
+    }
+  })
+  return NextResponse.json(currentUser)
+}
