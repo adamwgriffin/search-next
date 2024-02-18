fe326e9f0e37c979cc97916e1a0a8313e0aea8ab
Adam Griffin <adam.griffin@moxiworks.com>
2024-02-17 17:06:25 -0800

    Removed styled-jsx from SlideShow

--- components/listings/listing_detail/SlideShow/SlideShow.tsx
+++ components/listings/listing_detail/SlideShow/SlideShow.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next'
 import type { IPhotoGalleryImage } from '../../../../lib/types/listing_types'
 import { useState, useCallback } from 'react'
-import css from 'styled-jsx/css'
+import styles from './SlideShow.module.css'
 import Overlay from '../../../design_system/Overlay/Overlay'
 
 export interface SlideShowProps {
@@ -10,11 +10,7 @@ export interface SlideShowProps {
   onClose?: () => void
 }
 
-const SlideShow: NextPage<SlideShowProps> = ({
-  images,
-  open,
-  onClose
-}) => {
+const SlideShow: NextPage<SlideShowProps> = ({ images, open, onClose }) => {
   const [currentImage, setCurrentImage] = useState(0)
 
   const handleNextImage = useCallback(() => {
@@ -33,9 +29,9 @@ const SlideShow: NextPage<SlideShowProps> = ({
     <>
       {open && (
         <Overlay>
-          <div className='slideShow'>
-            <div className='header'>
-              <button className='close' onClick={onClose}>
+          <div className={styles.slideShow}>
+            <div className={styles.header}>
+              <button className={styles.close} onClick={onClose}>
                 X Close
               </button>
               <div>
@@ -43,24 +39,27 @@ const SlideShow: NextPage<SlideShowProps> = ({
               </div>
               <div>♡</div>
             </div>
-            <div className='carousel'>
-              <div className='carouselButtonConatiner'>
+            <div className={styles.carousel}>
+              <div>
                 <button
-                  className='carouselButton'
+                  className={styles.carouselButton}
                   onClick={handlePreviousImage}
                 >
                   &lt;
                 </button>
               </div>
-              <div className='listingImageContainer'>
+              <div>
                 <img
                   alt={`Listing Image ${currentImage}`}
                   src={images[currentImage].fullUrl}
-                  className='listingImage'
+                  className={styles.listingImage}
                 />
               </div>
-              <div className='carouselButtonConatiner'>
-                <button className='carouselButton' onClick={handleNextImage}>
+              <div>
+                <button
+                  className={styles.carouselButton}
+                  onClick={handleNextImage}
+                >
                   &gt;
                 </button>
               </div>
@@ -68,79 +67,8 @@ const SlideShow: NextPage<SlideShowProps> = ({
           </div>
         </Overlay>
       )}
-      <style jsx>{styles}</style>
     </>
   )
 }
-
-const styles = css`
-  .slideShow {
-    height: 100%;
-    display: grid;
-    grid-template-rows: auto 1fr;
-    color: white;
-  }
-
-  .carousel {
-    display: grid;
-    grid-template-columns: auto 1fr auto;
-    place-items: center;
-  }
-
-  .listingImage {
-    box-sizing: border-box;
-    display: block;
-    object-fit: contain;
-    width: 100%;
-    height: 100%;
-    margin: 0;
-    border: 0;
-    background: rgba(116, 245, 115, 0.18)
-  }
-
-  .header {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    padding: 2.5rem 2.5rem 1.25rem 2.5rem;
-  }
-
-  .close {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    cursor: pointer;
-    background: none;
-    border: none;
-    border-radius: 0.5rem;
-    color: inherit;
-    font: inherit;
-    padding: 0.5rem 1rem;
-  }
-
-  .close:hover {
-    background: rgb(74, 74, 74);
-  }
-
-  .carouselButton {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    cursor: pointer;
-    width: 3rem;
-    height: 3rem;
-    margin: 0 1.5rem;
-    padding: 0;
-    background: none;
-    border: solid 1px white;
-    border-radius: 50%;
-    color: inherit;
-    font: inherit;
-  }
-
-  .carouselButton:hover {
-    background: rgb(74, 74, 74);
-  }
-`
 
 export default SlideShow
