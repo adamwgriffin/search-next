79dd0b078133b3ba4a6c45039b55fc4e10c96532
Adam Griffin <adam.griffin@moxiworks.com>
2024-02-19 21:36:26 -0800

    Refactored ListingMarker
    * Using createPortal instead of createRoot to render the <ListingMarkerContect> JSX inside the marker instead of createRoot based on explanations and examples in https://react.dev/reference/react-dom/createPortal. It seems that I misunderstood what createRoot() does. It renders JSX as if it were part of an entirely separate React app, so any providers or context that wrap the parent in the root layout are not able pass data to the elements rendered inside of createRoot() function. Using createPortal() allows us to render children inside a DOM node other than the root node of the app but still behaves as if it were part of the same React app, and passes the context to the children as we want. We weren't able to use hooks like useSession inside <ListingMarkerContect> without this change.
    
    * Moved some code around and changes some variable names
    
    * Added some comments explaining how & why some things work

--- components/map/ListingMarker/ListingMarker.tsx
+++ components/map/ListingMarker/ListingMarker.tsx
@@ -1,7 +1,9 @@
+'use client'
+
 import type { NextPage } from 'next'
 import type { Listing } from '../../../lib/types/listing_types'
-import { useEffect, memo } from 'react'
-import { createRoot } from 'react-dom/client'
+import { useEffect, useState, memo } from 'react'
+import { createPortal } from 'react-dom'
 import { useGoogleMaps } from '../../../providers/GoogleMapsProvider'
 import ListingMarkerContent from '../ListingMarkerContent/ListingMarkerContent'
 import { listingLocationToLatLngLiteral } from '../../../lib/listing_helpers'
@@ -24,55 +26,70 @@ const ListingMarker: NextPage<ListingMarkerProps> = ({
   onClick
 }) => {
   const { googleMap } = useGoogleMaps()
+  const [markerContainer, setMarkerContainer] = useState<HTMLDivElement | null>(
+    null
+  )
 
   useEffect(() => {
     if (!googleMap) return
 
-    const handleMouseEnter = () => {
-      element.style.zIndex = '10000'
-      onMouseEnter?.(listing._id)
-    }
-
-    const handleMouseLeave = () => {
-      element.style.zIndex = zIndex.toString()
-      onMouseLeave?.()
-    }
-
-    const link = `/listing/${listing._id}`
-
     const markerContainer = document.createElement('div')
-    createRoot(markerContainer).render(
-      <ListingMarkerContent
-        listing={listing}
-        link={link}
-        highlighted={highlighted}
-      />
-    )
-
     const marker = new google.maps.marker.AdvancedMarkerElement({
       map: googleMap,
       position: listingLocationToLatLngLiteral(listing),
       content: markerContainer
     })
-
     marker.addListener('click', () => onClick?.(listing._id))
-    // there are only a few events that AdvancedMarkerElement supports, so we have to attach events to the element for
-    // others.
-    const element = marker.element as HTMLElement
-    element.style.zIndex = highlighted ? '10000' : zIndex.toString()
-    element.addEventListener('mouseenter', handleMouseEnter)
-    element.addEventListener('mouseleave', handleMouseLeave)
-
-    return () => {
-      marker.map = null
-      element.removeEventListener('mouseenter', handleMouseEnter)
-      element.removeEventListener('mouseleave', handleMouseLeave)
+    // there are currently only a few events that AdvancedMarkerElement supports, so we have to attach events to the
+    // element itself for others to work. Note: the element this returns appears to not be the same as the
+    // markerContainer div we created above since setting style attributes on it does not work, while it does with
+    // marker.element.
+    const markerElement = marker.element
+    markerElement.style.zIndex = highlighted ? '10000' : zIndex.toString()
+    const handleMouseEnter = () => {
+      markerElement.style.zIndex = '10000'
+      onMouseEnter?.(listing._id)
     }
-  }, [googleMap, listing, highlighted, zIndex, onMouseEnter, onMouseLeave, onClick])
+    const handleMouseLeave = () => {
+      markerElement.style.zIndex = zIndex.toString()
+      onMouseLeave?.()
+    }
+    markerElement.addEventListener('mouseenter', handleMouseEnter)
+    markerElement.addEventListener('mouseleave', handleMouseLeave)
 
-  return null
+    setMarkerContainer(markerContainer)
+
+    // Clean up by removing event listeners, removing the marker from the map and removing the div element we created
+    // from the DOM. Not sure if all of this is strictly necessary. We're doing it for now, just in case.
+    return () => {
+      markerElement.removeEventListener('mouseenter', handleMouseEnter)
+      markerElement.removeEventListener('mouseleave', handleMouseLeave)
+      marker.map = null
+      markerContainer.remove()
+    }
+  }, [
+    googleMap,
+    highlighted,
+    listing,
+    onClick,
+    onMouseEnter,
+    onMouseLeave,
+    zIndex
+  ])
+
+  if (markerContainer === null) return
+
+  return createPortal(
+    <ListingMarkerContent
+      listing={listing}
+      link={`/listing/${listing._id}`}
+      highlighted={highlighted}
+    />,
+    markerContainer
+  )
 }
 
+// Don't re-render the marker if all of these conditions are true
 const areEqual = (
   prevProps: Readonly<ListingMarkerProps>,
   nextProps: Readonly<ListingMarkerProps>
@@ -87,6 +104,6 @@ const areEqual = (
   )
 }
 
-// use the memo() HOC to avoid re-rendering markers on the map so it's more effecient and doesn't cause all the markers
+// use the memo() HOC to avoid re-rendering markers on the map so it's more effecient and doesn't cause every marker
 // to flicker each time the map is dragged
 export default memo(ListingMarker, areEqual)
