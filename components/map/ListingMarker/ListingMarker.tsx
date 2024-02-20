88544fec2adb6d49468fd7548f384465e3a41901
Adam Griffin <adam.griffin@moxiworks.com>
2024-02-20 15:08:17 -0800

    Extracted marker creation code into functions

--- components/map/ListingMarker/ListingMarker.tsx
+++ components/map/ListingMarker/ListingMarker.tsx
@@ -2,7 +2,7 @@
 
 import type { NextPage } from 'next'
 import type { Listing } from '../../../lib/types/listing_types'
-import { useEffect, useState, memo } from 'react'
+import { useEffect, useState, useCallback, memo } from 'react'
 import { createPortal } from 'react-dom'
 import { useGoogleMaps } from '../../../providers/GoogleMapsProvider'
 import ListingMarkerContent from '../ListingMarkerContent/ListingMarkerContent'
@@ -31,52 +31,59 @@ const ListingMarker: NextPage<ListingMarkerProps> = ({
     null
   )
 
+  const createMarker = useCallback(
+    (
+      markerContainer: HTMLElement
+    ): google.maps.marker.AdvancedMarkerElement => {
+      return new google.maps.marker.AdvancedMarkerElement({
+        map: googleMap,
+        position: listingLocationToLatLngLiteral(listing),
+        content: markerContainer
+      })
+    },
+    [googleMap, listing]
+  )
+
+  const addEventListenersToMarker = useCallback(
+    (
+      marker: google.maps.marker.AdvancedMarkerElement
+    ): { handleMouseEnter: () => void; handleMouseLeave: () => void } => {
+      marker.addListener('click', () => onClick?.(listing._id))
+      // there are currently only a few events that AdvancedMarkerElement supports, so we have to attach events to the
+      // element itself for others to work. Note: marker.element is not the the markerContainer we created
+      const markerElement = marker.element
+      markerElement.style.zIndex = highlighted ? '10000' : zIndex.toString()
+      const handleMouseEnter = () => {
+        markerElement.style.zIndex = '10000'
+        onMouseEnter?.(listing._id)
+      }
+      const handleMouseLeave = () => {
+        markerElement.style.zIndex = zIndex.toString()
+        onMouseLeave?.()
+      }
+      markerElement.addEventListener('mouseenter', handleMouseEnter)
+      markerElement.addEventListener('mouseleave', handleMouseLeave)
+      return { handleMouseEnter, handleMouseLeave }
+    },
+    [highlighted, listing._id, onClick, onMouseEnter, onMouseLeave, zIndex]
+  )
+
   useEffect(() => {
     if (!googleMap) return
-
     const markerContainer = document.createElement('div')
-    const marker = new google.maps.marker.AdvancedMarkerElement({
-      map: googleMap,
-      position: listingLocationToLatLngLiteral(listing),
-      content: markerContainer
-    })
-    marker.addListener('click', () => onClick?.(listing._id))
-    // there are currently only a few events that AdvancedMarkerElement supports, so we have to attach events to the
-    // element itself for others to work. Note: the element this returns appears to not be the same as the
-    // markerContainer div we created above since setting style attributes on it does not work, while it does with
-    // marker.element.
-    const markerElement = marker.element
-    markerElement.style.zIndex = highlighted ? '10000' : zIndex.toString()
-    const handleMouseEnter = () => {
-      markerElement.style.zIndex = '10000'
-      onMouseEnter?.(listing._id)
-    }
-    const handleMouseLeave = () => {
-      markerElement.style.zIndex = zIndex.toString()
-      onMouseLeave?.()
-    }
-    markerElement.addEventListener('mouseenter', handleMouseEnter)
-    markerElement.addEventListener('mouseleave', handleMouseLeave)
-
+    const marker = createMarker(markerContainer)
+    const { handleMouseEnter, handleMouseLeave } =
+      addEventListenersToMarker(marker)
     setMarkerContainer(markerContainer)
-
     // Clean up by removing event listeners, removing the marker from the map and removing the div element we created
     // from the DOM. Not sure if all of this is strictly necessary. We're doing it for now, just in case.
     return () => {
-      markerElement.removeEventListener('mouseenter', handleMouseEnter)
-      markerElement.removeEventListener('mouseleave', handleMouseLeave)
+      marker.element.removeEventListener('mouseenter', handleMouseEnter)
+      marker.element.removeEventListener('mouseleave', handleMouseLeave)
       marker.map = null
       markerContainer.remove()
     }
-  }, [
-    googleMap,
-    highlighted,
-    listing,
-    onClick,
-    onMouseEnter,
-    onMouseLeave,
-    zIndex
-  ])
+  }, [addEventListenersToMarker, createMarker, googleMap])
 
   if (markerContainer === null) return
 
@@ -91,7 +98,7 @@ const ListingMarker: NextPage<ListingMarkerProps> = ({
 }
 
 // Don't re-render the marker if all of these conditions are true
-const areEqual = (
+const propsAreEqual = (
   prevProps: Readonly<ListingMarkerProps>,
   nextProps: Readonly<ListingMarkerProps>
 ) => {
@@ -105,4 +112,4 @@ const areEqual = (
 
 // use the memo() HOC to avoid re-rendering markers on the map so it's more effecient and doesn't cause every marker
 // to flicker each time the map is dragged
-export default memo(ListingMarker, areEqual)
+export default memo(ListingMarker, propsAreEqual)
