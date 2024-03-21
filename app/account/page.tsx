f4be470e248ae27352d5cec45359556be380345a
Adam Griffin <adam.griffin@moxiworks.com>
2024-03-20 22:44:48 -0700

    Added placeholder page for My Account

---
+++ app/account/page.tsx
@@ -0,0 +1,11 @@
+export default function Account() {
+  return (
+    <div>
+      <h1>My Account</h1>
+      <p>
+        Login required to reach this page. Defined in middleware.ts
+        config.matcher.
+      </p>
+    </div>
+  )
+}
