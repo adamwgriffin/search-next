c62321eaa808438e9abf5bcf8b02e35cef196d2d
Adam Griffin <ag@agriffin.me>
2024-07-21 14:45:27 -0700

    Removed SavedSearchCreateRequest type
    No idea why I added this in the first place. It's doesn't seem to actually do anything.

--- app/api/saved_search/route.ts
+++ app/api/saved_search/route.ts
@@ -1,12 +1,9 @@
-import type { SavedSearch } from '@prisma/client'
 import { NextResponse } from 'next/server'
 import prisma from '../../../lib/prismadb'
 
-export type SavedSearchCreateRequest = Pick<SavedSearch, 'userId' | 'name' | 'searchParams' | 'messageCadence'>
-
 export async function POST(request: Request) {
   const { id } = await prisma.savedSearch.create({
-    data: await request.json() as SavedSearchCreateRequest
+    data: await request.json()
   })
   return NextResponse.json(
     { id },
