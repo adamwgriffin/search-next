b6ce0371ba31ce8f68716021519b23fda0acbc6d
Adam Griffin <ag@agriffin.me>
2024-07-21 14:58:51 -0700

    Finished remaining CRUD for saved search endpoints

--- app/api/saved_search/[id]/route.ts
+++ app/api/saved_search/[id]/route.ts
@@ -6,6 +6,41 @@ export interface SavedSearchParams {
   id: string
 }
 
+export async function GET(
+  _request: Request,
+  { params }: { params: SavedSearchParams }
+) {
+  const savedSearch = await prisma.savedSearch.findUnique({
+    where: {
+      id: params.id
+    }
+  })
+  return savedSearch ?
+    NextResponse.json(savedSearch) :
+    new NextResponse(null, { status: 404 })
+}
+
+export async function PUT(
+  request: Request,
+  { params }: { params: SavedSearchParams }
+) {
+  try {
+    await prisma.savedSearch.update({
+      where: {
+        id: params.id
+      },
+      data: await request.json()
+    })
+    return new NextResponse(null, { status: 200 })
+  } catch (error) {
+    console.error("Error updating saved search:", error)
+    return NextResponse.json(
+      { message: 'Error updating saved search' },
+      { status: 422 }
+    )
+  }
+}
+
 export async function DELETE(
   _request: Request,
   { params }: { params: SavedSearchParams }
@@ -18,7 +53,7 @@ export async function DELETE(
     })
     return new NextResponse(null, { status: 200 })
   } catch (error) {
-    console.log("Error deleting saved search:", error)
+    console.error("Error deleting saved search:", error)
     if (error instanceof PrismaClientKnownRequestError && error.code === 'P2025') {
       return NextResponse.json(
         { error: 'Saved search not found' },
