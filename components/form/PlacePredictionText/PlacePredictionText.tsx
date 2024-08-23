90172eee4759a7e882222cf31bbc83e8356e22dc
Adam Griffin <ag@agriffin.me>
2024-08-23 16:42:01 -0700

    Fixed bug & refactored PlacePredictionText
    Apparently the Google Maps Autocomplete Service will sometimes return an autocomplete prediction that does not include a "main_text_matched_substrings" attribute in its structured_formatting attribute but the StructuredFormatting type does not indicate that this field is optional. Calling flatMap on this attribute when it's undefined was causing us to get into yet another endless loop. The fix was to just return if the attribute is not present. I also put the function that gets the value inside of a useMemo hook so that reference to the substrings array will remain stable and not re-render over and over, as it was doing.

--- components/form/PlacePredictionText/PlacePredictionText.tsx
+++ components/form/PlacePredictionText/PlacePredictionText.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from 'next'
+import { useMemo } from 'react'
 import uniq from 'lodash/uniq'
 import styles from './PlacePredictionText.module.css'
 
@@ -6,34 +7,48 @@ export interface PlacePredictionTextProps {
   prediction: google.maps.places.AutocompletePrediction
 }
 
-const PlacePredictionText: NextPage<PlacePredictionTextProps> = ({ prediction }) => {
+const getSubStringObjs = (
+  matches: google.maps.places.PredictionSubstring[],
+  str: string
+) => {
+  const matchOffsets = matches.flatMap(({ offset, length }) => [
+    offset,
+    offset + length
+  ])
+  const allOffsets = uniq([0, ...matchOffsets, str.length])
+  const allOffsetPairs = allOffsets
+    .slice(1)
+    .map((end, i) => [allOffsets[i], end])
+  return allOffsetPairs.map(([start, end]) => {
+    const text = str.substring(start, end)
+    const match = !!matches.find(
+      ({ offset, length }) => offset === start && offset + length === end
+    )
+    return { text, match }
+  })
+}
 
-  const getSubStringObjs = (matches: google.maps.places.PredictionSubstring[], str: string) => {
-    const matchOffsets = matches.flatMap(({ offset, length }) => [offset, offset+length])
-    const allOffsets = uniq([0, ...matchOffsets, str.length])
-    const allOffsetPairs = allOffsets.slice(1).map((end, i) => [allOffsets[i], end])
-    return allOffsetPairs.map(([start, end]) => {
-      const text = str.substring(start, end)
-      const match = !!matches.find(({ offset, length }) => (offset === start && offset+length === end))
-      return { text, match }
-    })
-  }
-
-  const mainTextSubstrings = () => {
-    const { main_text_matched_substrings, main_text } = prediction.structured_formatting
-    const subs = getSubStringObjs(main_text_matched_substrings, main_text)
-    return subs
-  }
+const PlacePredictionText: NextPage<PlacePredictionTextProps> = ({
+  prediction
+}) => {
+  const mainTextSubstrings = useMemo(() => {
+    const { main_text_matched_substrings, main_text } =
+      prediction.structured_formatting
+    if (!main_text_matched_substrings) {
+      return [{ text: main_text, match: false }]
+    }
+    return getSubStringObjs(main_text_matched_substrings, main_text)
+  }, [prediction.structured_formatting])
 
   return (
     <span className='placesPredictionText'>
       <span className={styles.mainText}>
-        {mainTextSubstrings().map((string) => (
+        {mainTextSubstrings.map((substringMatch) => (
           <span
-            key={string.text}
-            className={string.match ? styles.matched : ''}
+            key={substringMatch.text}
+            className={substringMatch.match ? styles.matched : ''}
           >
-            { string.text }
+            {substringMatch.text}
           </span>
         ))}
       </span>
