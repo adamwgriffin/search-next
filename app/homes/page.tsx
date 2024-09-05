31bd7a6d0a4d7e25e12a9a2fdea9f0127bcf8b33
Adam Griffin <ag@agriffin.me>
2024-09-05 13:17:04 -0700

    Comments

--- app/homes/page.tsx
+++ app/homes/page.tsx
@@ -40,15 +40,15 @@ const SearchPage: NextPage<SearchPageProps> = () => {
   const [previousSearchState, setPreviousSearchState] =
     useState<Partial<FiltersState>>()
 
-  // get the browser url query string, convert it to a state object, use it to set the state, then run a new search
+  // Get the browser url query string, convert it to a state object, use it to set the state, then run a new search
   // based on that state.
-  // TODO: we will eventually validate query params with zod and throw away any that are invalid, so that we can
+  // TODO: We will eventually validate query params with zod and throw away any that are invalid, so that we can
   // guarantee that this function will always be passed valid data
   const getSearchParamsAndSetSearchState =
     useCallback((): Partial<FiltersState> => {
-      // we have to include all the intial state and then override those defaults with the params from the url. this is
-      // necessary because the url only includes params that are different from the default filters. it is possible for
-      // the app's filters to have been changed from the default but then a brand new url is entered. in that scenario
+      // We have to include all the intial state and then override those defaults with the params from the url. This is
+      // necessary because the url only includes params that are different from the default filters. It is possible for
+      // the app's filters to have been changed from the default but then a brand new url is entered. In that scenario
       // the internal state and the url params may get out of sync.
       const newSearchState = {
         ...initialState,
@@ -60,8 +60,8 @@ const SearchPage: NextPage<SearchPageProps> = () => {
       return newSearchState
     }, [dispatch])
 
+  // This useEffect is the entrypoint for getting params from the url and running the first search after the page loads
   useEffect(() => {
-    // run a search based on the url on initial render
     getSearchParamsAndSetSearchState()
     searchNewLocation()
 
@@ -72,8 +72,8 @@ const SearchPage: NextPage<SearchPageProps> = () => {
     }
   }, [dispatch, getSearchParamsAndSetSearchState, searchNewLocation])
 
-  // if the user clicks the back or forward button in the browser, we want to get the url that was loaded from the
-  // previous/next part of the browser history, and then run a new search to match the url params. the "popstate" event
+  // If the user clicks the back or forward button in the browser, we want to get the url that was loaded from the
+  // previous/next part of the browser history, and then run a new search to match the url params. The "popstate" event
   // is triggered whenever the history is changed by the user in this way.
   const onPopState = useCallback(() => {
     const newSearchState = getSearchParamsAndSetSearchState()
@@ -94,17 +94,17 @@ const SearchPage: NextPage<SearchPageProps> = () => {
 
   useEvent('popstate', onPopState)
 
-  // each time the user triggers a new search, we want to update the url, so that if the user were to visit this new
-  // url, it would load this specific search. we're checking initialSearchComplete here because we don't want to change
-  // the url if we just used it to set the searchState and run the initial search in the useMount callback. it's only on
-  // subsequent runs that we would want to update the url to reflect whatever filters the user has changed. doing this
-  // prevents us from adding a duplicate url to the browser history, which can make if seem as if the back button
-  // doesn't work if the user clicks it after the search page first loads
+  // Each time the user triggers a new search, we want to update the url, so that if the user were to go directly to
+  // this new url in the future, it would load this specific search. We're checking initialSearchComplete here because
+  // we don't want to change the url if we just used it to set the searchState and run the initial search in the other
+  // useEffect callback above. It's only on subsequent runs that we would want to update the url to reflect whatever
+  // filters the user has changed. Doing this prevents us from adding a duplicate url to the browser history, which can
+  // make if seem as if the back button doesn't work if the user clicks it after the search page first loads.
   useEffect(() => {
     const shouldUpdateURL =
       listingSearchRunning &&
       initialSearchComplete &&
-      // avoid updating unless the searchState changed, otherwise clicking the back button will not change the url
+      // Avoid updating unless the searchState changed, otherwise clicking the back button will not change the url
       !isEqual(searchState, previousSearchState)
     if (shouldUpdateURL) {
       searchWithFilterState(searchState)
