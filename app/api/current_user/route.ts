51816b7b79969dc60c2d5044f8b13e41a43e266a
Adam Griffin <ag@agriffin.me>
2024-10-01 14:16:44 -0700

    Workaround for Typescript build error

--- app/api/current_user/route.ts
+++ app/api/current_user/route.ts
@@ -1,12 +1,21 @@
+import type { User } from '@prisma/client'
 import { NextResponse } from 'next/server'
 import { getServerSession } from 'next-auth/next'
 import authOptions from '../../../lib/auth_options'
 import prisma from '../../../lib/prismadb'
 
-export async function GET() {
+// The findUnique method returns a subset of the entire User type
+export type UserQueryResult = Pick<
+  User,
+  'id' | 'name' | 'email' | 'image' | 'image' | 'favoriteIds'
+> | null
+
+// There's currently a bug in the type definitions for GET that causes a Typescript error when we build the project.
+// Explicitly typing the return value for the function allows us to work around this.
+export async function GET(): Promise<NextResponse<UserQueryResult>> {
   const session = await getServerSession(authOptions)
   if (!session?.user) {
-    return null
+    return NextResponse.json(null)
   }
   const currentUser = await prisma.user.findUnique({
     where: {
