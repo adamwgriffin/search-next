7e6c2968306024ff33cafe6d835e1277125e49c0
Adam Griffin <ag@agriffin.me>
2024-11-14 14:16:30 -0800

    More fixes for image display on mobile

--- components/listings/listing_detail/ListingDetailImage/ListingDetailImage.tsx
+++ components/listings/listing_detail/ListingDetailImage/ListingDetailImage.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next'
 import type { ListingDetail } from '../../../../types/listing_types'
-import styles from './ListingDetailImage.module.css'
+import { useMedia } from 'react-use'
 import ListingImageContainer from '../../ListingImageContainer/ListingImageContainer'
 import ListingImageContainerElements from '../../ListingImageContainerElements/ListingImageContainerElements'
 import ListingMainImage from '../../ListingMainImage/ListingMainImage'
@@ -15,28 +15,28 @@ const ListingDetailImage: NextPage<ListingDetailImageProps> = ({
   listing,
   onClick
 }) => {
+  const isSmallAndUp = useMedia('(min-width: 576px)', false)
+
   return (
-    <div className={styles.listingDetailImage}>
-      <ListingImageContainer>
-        <ListingImageContainerElements>
-          <FavoriteButton listingId={listing._id} />
-        </ListingImageContainerElements>
-        <ListingMainImage
-          image={listing?.photoGallery?.[0]}
-          latitude={listing.latitude}
-          longitude={listing.longitude}
-          size='full'
-          style={{
-            objectFit: 'cover',
-            cursor: 'pointer',
-            width: '100%',
-            height: '100%',
-            borderRadius: '0.5rem'
-          }}
-          onClick={onClick}
-        />
-      </ListingImageContainer>
-    </div>
+    <ListingImageContainer>
+      <ListingImageContainerElements>
+        <FavoriteButton listingId={listing._id} />
+      </ListingImageContainerElements>
+      <ListingMainImage
+        image={listing?.photoGallery?.[0]}
+        latitude={listing.latitude}
+        longitude={listing.longitude}
+        size='full'
+        style={{
+          objectFit: 'cover',
+          cursor: 'pointer',
+          width: '100%',
+          height: isSmallAndUp ? '30rem' : '20rem',
+          borderRadius: '0.5rem'
+        }}
+        onClick={onClick}
+      />
+    </ListingImageContainer>
   )
 }
 
