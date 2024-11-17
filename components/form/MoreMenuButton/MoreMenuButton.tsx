a46e11d985061cc55475d519eb2f61b794cee8c7
Adam Griffin <ag@agriffin.me>
2024-11-16 17:43:53 -0800

    #31 - Add clear all filters button on desktop

--- components/form/MoreMenuButton/MoreMenuButton.tsx
+++ components/form/MoreMenuButton/MoreMenuButton.tsx
@@ -1,11 +1,17 @@
 import { useState } from 'react'
+import { useAppDispatch } from '../../../hooks/app_hooks'
+import { clearFilters } from '../../../store/filters/filtersSlice'
+import { searchWithUpdatedFilters } from '../../../store/listingSearch/listingSearchCommon'
 import MenuContainter from '../../design_system/MenuContainter/MenuContainter'
 import ToggleOpenButton from '../../design_system/ToggleOpenButton/ToggleOpenButton'
 import More from '../../../containers/More/More'
-import FiltersFooter from '../../../containers/FiltersFooter/FiltersFooter'
+import Footer from '../../design_system/Footer/Footer'
+import TextButton from '../../design_system/TextButton/TextButton'
+import ContainedButton from '../../design_system/ContainedButton/ContainedButton'
 import styles from './MoreMenuButton.module.css'
 
 const MoreMenuButton: React.FC = () => {
+  const dispatch = useAppDispatch()
   const [open, setOpen] = useState(false)
 
   return (
@@ -19,6 +25,19 @@ const MoreMenuButton: React.FC = () => {
         <div className={styles.content}>
           <More />
         </div>
+        <Footer>
+          <TextButton
+            onClick={() => {
+              dispatch(clearFilters())
+              dispatch(searchWithUpdatedFilters())
+            }}
+          >
+            Clear all
+          </TextButton>
+          <ContainedButton onClick={() => setOpen(false)}>
+            Done
+          </ContainedButton>
+        </Footer>
       </div>
     </MenuContainter>
   )
