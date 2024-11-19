ca2daaacbbcce90fb2475dad98f08be693d62c18
Adam Griffin <ag@agriffin.me>
2024-11-18 20:52:36 -0800

    Refactored SaveSearchButton
    Put all its dependent code inside of the button itself

--- containers/Filters/Filters.tsx
+++ containers/Filters/Filters.tsx
@@ -1,9 +1,8 @@
 import type { NextPage } from 'next'
-import { useSession } from 'next-auth/react'
 import { useAppSelector, useAppDispatch } from '../../hooks/app_hooks'
 import {
   selectViewType,
-  setViewType,
+  setViewType
 } from '../../store/application/applicationSlice'
 import styles from './Filters.module.css'
 import PriceMenuButton from '../../components/form/PriceMenuButton/PriceMenuButton'
@@ -11,30 +10,19 @@ import BedsAndBathsMenuButton from '../../components/form/BedsAndBathsMenuButton
 import MoreMenuButton from '../../components/form/MoreMenuButton/MoreMenuButton'
 import FiltersButton from '../../components/form/FiltersButton/FiltersButton'
 import ViewSwitcher from '../../components/form/ViewSwitcher/ViewSwitcher'
-import OutlinedButton from '../../components/design_system/OutlinedButton/OutlinedButton'
+import SaveSearchButton from '../../components/form/SaveSearchButton/SaveSearchButton'
 
 const Filters: NextPage = () => {
-  const { data: session } = useSession()
   const dispatch = useAppDispatch()
   const viewType = useAppSelector(selectViewType)
-
-  const handleSaveSearch = useCallback(() => {
-    if (session?.user) {
-      dispatch(openModal({ modalType: 'saveSearch' }))
-    } else {
-      dispatch(openModal({ modalType: 'loginOrRegister' }))
-    }
-  }, [dispatch, session?.user])
 
   return (
     <div className={styles.filters}>
       <PriceMenuButton />
       <BedsAndBathsMenuButton />
       <MoreMenuButton />
-      <OutlinedButton textColor='var(--primary)' onClick={handleSaveSearch}>
-        Save Search
-      </OutlinedButton>
       <FiltersButton />
+      <SaveSearchButton />
       <ViewSwitcher
         viewType={viewType}
         onClick={() => {
