24bf1f56a1c4a621891e8619d95f67ecf9f6d429
Adam Griffin <ag@agriffin.me>
2024-12-11 23:47:19 -0800

    Moved slideshow into ListingDetailImage
    Attempting to move client components further out to the leaf nodes of the component tree.

--- components/listings/listing_detail/ListingDetailImage.tsx
+++ components/listings/listing_detail/ListingDetailImage.tsx
@@ -1,30 +1,53 @@
+'use client'
+
 import type { ListingDetail } from '../../../types/listing_types'
+import { useState, useEffect } from 'react'
+import { createPortal } from 'react-dom'
 import ListingImageContainer from '../ListingImageContainer/ListingImageContainer'
 import ListingImageContainerElements from '../ListingImageContainerElements/ListingImageContainerElements'
 import FavoriteButton from '../../../containers/FavoriteButton/FavoriteButton'
 import ListingDetailMainImage from './ListingDetailMainImage/ListingDetailMainImage'
+import SlideShow from './SlideShow/SlideShow'
 
 export type ListingDetailImageProps = {
   listing: ListingDetail
-  onClick?: () => void
 }
 
-const ListingDetailImage: React.FC<ListingDetailImageProps> = ({
-  listing,
-  onClick
-}) => {
+const ListingDetailImage: React.FC<ListingDetailImageProps> = ({ listing }) => {
+  const [isMounted, setIsMounted] = useState(false)
+  const [slideShowOpen, setSlideShowOpen] = useState(false)
+
+  useEffect(() => {
+    setIsMounted(true)
+  }, [])
+
   return (
-    <ListingImageContainer>
-      <ListingImageContainerElements>
-        <FavoriteButton listingId={listing._id} />
-      </ListingImageContainerElements>
-      <ListingDetailMainImage
-        imageUrl={listing.photoGallery?.[0]?.url}
-        streetViewLat={listing.latitude}
-        streetViewLng={listing.longitude}
-        onClick={onClick}
-      />
-    </ListingImageContainer>
+    <>
+      <ListingImageContainer>
+        <ListingImageContainerElements>
+          <FavoriteButton listingId={listing._id} />
+        </ListingImageContainerElements>
+        <ListingDetailMainImage
+          imageUrl={listing.photoGallery?.[0]?.url}
+          streetViewLat={listing.latitude}
+          streetViewLng={listing.longitude}
+          onClick={() => setSlideShowOpen(true)}
+        />
+      </ListingImageContainer>
+      {
+        // We're checking isMounted here because document.body doesn't exist
+        // when this is first rendered on the server, which causes an error
+        isMounted &&
+          createPortal(
+            <SlideShow
+              open={slideShowOpen}
+              onClose={() => setSlideShowOpen(false)}
+              images={listing.photoGallery || []}
+            />,
+            document.body
+          )
+      }
+    </>
   )
 }
 
