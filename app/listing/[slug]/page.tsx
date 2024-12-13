a35b66fd6bdbf4fa76177d46052356a8d8a6c3e2
Adam Griffin <ag@agriffin.me>
2024-12-13 13:54:43 -0800

    Removed useGetCurrentUserIfAuthenticated hook
    It seems to do nothing.

--- app/listing/[slug]/page.tsx
+++ app/listing/[slug]/page.tsx
@@ -4,7 +4,6 @@ import type { ListingDetailParams } from '../../api/listing_detail/[slug]/route'
 import { useEffect } from 'react'
 import { useParams } from 'next/navigation'
 import { useAppSelector, useAppDispatch } from '../../../hooks/app_hooks'
-import { useGetCurrentUserIfAuthenticated } from '../../../hooks/get_current_user_if_authenticated_hook'
 import {
   getListingDetail,
   selectListing
@@ -15,7 +14,6 @@ import ListingDetail from '../../../components/listings/listing_detail/ListingDe
 import LoginOrRegisterModal from '../../../containers/modals/LoginOrRegisterModal/LoginOrRegisterModal'
 
 const ListingPage: React.FC = () => {
-  useGetCurrentUserIfAuthenticated()
   const params = useParams<ListingDetailParams>()
   const dispatch = useAppDispatch()
   const listing = useAppSelector(selectListing)
