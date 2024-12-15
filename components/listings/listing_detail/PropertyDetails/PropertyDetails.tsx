6ba78e9c6f0cc9a59d22caf3309d22c0804bfcbb
Adam Griffin <ag@agriffin.me>
2024-12-14 23:43:52 -0800

    Removed ShowMore
    Seems like clamping the property details text like this is actually pretty pointless.

--- components/listings/listing_detail/PropertyDetails/PropertyDetails.tsx
+++ components/listings/listing_detail/PropertyDetails/PropertyDetails.tsx
@@ -1,7 +1,6 @@
 import type { PropertDetailsSection } from '../../../../types/listing_types'
 import { Fragment } from 'react'
 import styles from './PropertyDetails.module.css'
-import ShowMore from '../../../design_system/ShowMore/ShowMore'
 
 export type PropertyDetailsProps = {
   propertyDetails: PropertDetailsSection[]
@@ -11,7 +10,7 @@ const PropertyDetails: React.FC<PropertyDetailsProps> = ({
   propertyDetails
 }) => {
   return (
-    <ShowMore>
+    <>
       <h4>Property Details</h4>
       {propertyDetails.map(({ _id, name, details }) => (
         <Fragment key={_id}>
@@ -27,7 +26,7 @@ const PropertyDetails: React.FC<PropertyDetailsProps> = ({
           </ul>
         </Fragment>
       ))}
-    </ShowMore>
+    </>
   )
 }
 
